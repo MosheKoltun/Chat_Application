efe23d992e2ac2da48d80199c69ea26d7862fb99
unknown <muni_koltun@yahoo.com>
2018-05-02 09:56:17 +0300

    All Features are working. need to add input validation for User Age

--- Modules/chat.js
+++ Modules/chat.js
@@ -5,26 +5,53 @@ const groupFuncs = require('./groups.js');
 module.exports = {
     addUserToGroup:addUserToGroup,
     removeUserFromGroup:removeUserFromGroup,
-    getListOfGroupAndUsers:getListOfGroupAndUsers,
+    createGroupsUsersDisplayTree:createGroupsUsersDisplayTree,
 };
 //=========================================================
 // addUserToGroup
 //=========================================================
 function addUserToGroup(groupName, username){
+    // check that there is no such group in the system at all
+    // (Inside the array kept in groups.js called 'groupObjectList')
     var groupObject = groupFuncs.doesGroupExist(groupName);
     if (groupObject!=null) {
+        // check that there is no such user in the system at all
+        // (Inside the array kept in users.js called 'userObjectList')
         var userObject = userFuncs.doesUserExist(username);
         if (userObject!=null) {
-            groupFuncs.addUserToGroup(groupName, userObject);
-            return true;
+            var res = doesUserExistInGroupsUsersDisplayTree(username);
+            // If user is not is group users
+            if (!res) { // user is not is group users
+                groupFuncs.addUserToGroup(groupName, userObject);
+                return true;
+            }
         }
     }
     return false;
 }
 //=========================================================
+// doesUserExistInGroupsUsersDisplayTree
+//=========================================================
+function doesUserExistInGroupsUsersDisplayTree(input) {
+    var listOfGroupAndUsers = createGroupsUsersDisplayTree();
+
+    for (var groupName in listOfGroupAndUsers) {
+
+        var listOfUsernamesAndAges = listOfGroupAndUsers[groupName];
+
+        for (var username in listOfUsernamesAndAges) {
+            if (username === input) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
+//=========================================================
 // removeUserFromGroup
 //=========================================================
 function removeUserFromGroup(groupName, username){
+
     var groupObject = groupFuncs.doesGroupExist(groupName);
     if (groupObject===null) {
         return false;
@@ -36,11 +63,18 @@ function removeUserFromGroup(groupName, username){
     groupFuncs.removeUserFromGroup(groupName, userObject);
     return true;
 }
+
 //=========================================================
-// getListOfGroupAndUsers
+// createGroupsUsersDisplayTree
 //=========================================================
-function getListOfGroupAndUsers(){
-    var listOfGroupAndUsers = {};
+function createGroupsUsersDisplayTree(){
+    //This Function returns a tree of strings
+    //Only group names and username and ages
+    //notice: it does not return objects
+
+    var groupsUsersDisplayTree = {};
+    // {"groupName" : {"username" : "("+age+")"}}
+    // example: {group1 : {user1 : (34)}, group2 : {user2: (14), user3 : (50)}}
 
     var groupObjectList = groupFuncs.getGroupObjectList();
 
@@ -58,7 +92,7 @@ function getListOfGroupAndUsers(){
 
             listOfUsernamesAndAges[username]=userAge;
         }
-        listOfGroupAndUsers[groupName]=listOfUsernamesAndAges;
+        groupsUsersDisplayTree[groupName]=listOfUsernamesAndAges;
     }
-    return listOfGroupAndUsers;
+    return groupsUsersDisplayTree;
 }
